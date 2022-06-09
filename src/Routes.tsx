af07c94be8b3a0f99a5120f344d4a442b535316d
nguyen minh duc <mducnguyen76xx@gmail.com>
2022-06-09 11:29:31 +0700

    first cmt

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -5,7 +5,7 @@ import ProtectedRoute from './modules/common/components/ProtectedRoute';
 
 const HomePage = lazy(() => import('./modules/home/pages/HomePage'));
 const ContactPage = lazy(() => import('./modules/home/pages/ContactPage'));
-const LoginPage = lazy(() => import('./modules/auth/pages/LoginPage'));
+const SecondLoginPage = lazy(() => import('./modules/auth/pages/SecondLoginPage'));
 
 interface Props {}
 
@@ -15,11 +15,11 @@ export const Routes = (props: Props) => {
   return (
     <Suspense fallback={<div>Loading.....</div>}>
       <Switch location={location}>
-        <Route path={ROUTES.login} component={LoginPage} />
+        <Route path={ROUTES.login} component={SecondLoginPage} />
         <ProtectedRoute path={ROUTES.home} component={HomePage} />
         <Route path={ROUTES.contact} component={ContactPage} />
 
-        <Route path="/" component={LoginPage} />
+        <Route path="/" component={SecondLoginPage} />
       </Switch>
     </Suspense>
   );
--- src/modules/auth/pages/SecondLoginPage.tsx
+++ src/modules/auth/pages/SecondLoginPage.tsx
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
-import LoginForm from '../components/LoginForm';
+import './SecondLoginPage.scss';
 import logo from '../../../logo-420-x-108.png';
-import { ILoginParams } from '../../../models/auth';
+import SecondLoginForm from '../components/SecondLoginForm';
 import { useDispatch } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk';
 import { AppState } from '../../../redux/reducer';
-import { Action } from 'redux';
-import { fetchThunk } from '../../common/redux/thunk';
+import { Action } from 'typesafe-actions';
+import { ILoginParams } from '../../../models/auth';
 import { API_PATHS } from '../../../configs/api';
-import { RESPONSE_STATUS_SUCCESS } from '../../../utils/httpResponseCode';
+import { fetchThunk } from '../../common/redux/thunk';
 import { setUserInfo } from '../redux/authReducer';
-import Cookies from 'js-cookie';
+import { RESPONSE_STATUS_SUCCESS } from '../../../utils/httpResponseCode';
 import { ACCESS_TOKEN_KEY } from '../../../utils/constants';
-import { ROUTES } from '../../../configs/routes';
+import Cookies from 'js-cookie';
 import { replace } from 'connected-react-router';
+import { ROUTES } from '../../../configs/routes';
 import { getErrorMessageResponse } from '../../../utils';
+interface Props {}
 
-const LoginPage = () => {
+const SecondLoginPage = (props: Props) => {
   const dispatch = useDispatch<ThunkDispatch<AppState, null, Action<string>>>();
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
@@ -25,36 +27,31 @@ const LoginPage = () => {
     async (values: ILoginParams) => {
       setErrorMessage('');
       setLoading(true);
+
       const json = await dispatch(
         fetchThunk(API_PATHS.signIn, 'post', { email: values.email, password: values.password }),
       );
+
       setLoading(false);
+
       if (json?.code === RESPONSE_STATUS_SUCCESS) {
         dispatch(setUserInfo(json.data));
         Cookies.set(ACCESS_TOKEN_KEY, json.data.token, { expires: values.rememberMe ? 7 : undefined });
         dispatch(replace(ROUTES.home));
         return;
       }
+
       setErrorMessage(getErrorMessageResponse(json));
     },
     [dispatch],
   );
 
   return (
-    <div
-      className="container"
-      style={{
-        height: '100vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column',
-      }}
-    >
-      <img src={logo} alt="" style={{ maxWidth: '250px', margin: '32px' }} />
-      <LoginForm onLogin={onLogin} loading={loading} errorMessage={errorMessage} />
+    <div className="secondloginpage container">
+      <img className="secondloginpage-logo" src={logo} alt="" />
+      <SecondLoginForm onLogin={onLogin} loading={loading} errorMessage={errorMessage} />
     </div>
   );
 };
 
-export default LoginPage;
+export default SecondLoginPage;
